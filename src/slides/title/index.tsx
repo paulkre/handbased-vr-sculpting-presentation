8af9127df27437d646a61f490558c64fec58f156
Paul Kretschel <paul@kretschel.media>
2020-01-25 13:40:59 +0100

    Add colors to title slide

--- src/slides/title/index.tsx
+++ src/slides/title/index.tsx
@@ -3,17 +3,20 @@ import React from "react";
 import { Frame } from "../../components/frame";
 
 import styles from "./title.module.scss";
+import logosUrl from "./hsd-logos.svg";
 
 export const Title: React.FC = () => (
   <Frame>
     <div className={styles.wrapper}>
-      <span className={styles.label}>Kolloquium zur Bachelor Arbeit</span>
-      <div className={styles.separator} />
+      <div className={styles.label}>
+        Kolloquium zur Bachelor Arbeit von Paul Kretschel
+      </div>
       <span className={styles.heading}>
         Handbasiertes Sculpting
         <br />
         in Virtual Reality
       </span>
+      <img src={logosUrl} alt="HSD Logos" className={styles.logos} />
     </div>
   </Frame>
 );
