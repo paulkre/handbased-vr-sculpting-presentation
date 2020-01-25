79d60f708553eec188073312d158dd3af5dfc50a
Paul Kretschel <paul@kretschel.media>
2020-01-25 20:34:13 +0100

    Remove animations from title slide

--- src/slides/title/index.tsx
+++ src/slides/title/index.tsx
@@ -1,39 +1,22 @@
-import React, { useState } from "react";
+import React from "react";
 
 import { Frame } from "../../components/frame";
-import { FadeIn } from "../../components/fade-in";
-import { IntervalList } from "../../components/interval-list";
 
 import styles from "./title.module.scss";
 import logosUrl from "./hsd-logos.svg";
 
-export const Title: React.FC = () => {
-  const [animationFinished, setAnimationFinished] = useState(false);
-
-  const handleAnimationFinished = () => setAnimationFinished(true);
-
-  const style: React.CSSProperties = {};
-  if (!animationFinished) style.borderColor = "rgba(1,1,1,0)";
-
-  return (
-    <Frame>
-      <div className={styles.outerWrapper} style={style}>
-        <div className={styles.innerWrapper}>
-          <IntervalList
-            intervalDuration={0.2}
-            wrapElement={<FadeIn duration={3} />}
-            onFinished={handleAnimationFinished}
-          >
-            <div className={styles.label}>Kolloquium von Paul Kretschel</div>
-            <span className={styles.heading}>
-              Handbasiertes Sculpting
-              <br />
-              in Virtual Reality
-            </span>
-            <img src={logosUrl} alt="HSD Logos" className={styles.logos} />
-          </IntervalList>
-        </div>
+export const Title: React.FC = () => (
+  <Frame>
+    <div className={styles.outerWrapper}>
+      <div className={styles.innerWrapper}>
+        <div className={styles.label}>Kolloquium von Paul Kretschel</div>
+        <span className={styles.heading}>
+          Handbasiertes Sculpting
+          <br />
+          in Virtual Reality
+        </span>
+        <img src={logosUrl} alt="HSD Logos" className={styles.logos} />
       </div>
-    </Frame>
-  );
-};
+    </div>
+  </Frame>
+);
