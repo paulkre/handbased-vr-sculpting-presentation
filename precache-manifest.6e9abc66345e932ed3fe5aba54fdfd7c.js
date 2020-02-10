9664b7a9d48bef3012448fc1d4f7c7606f14e26c
Paul Kretschel <paul@kretschel.media>
2020-02-10 13:14:57 +0100

    Updates

---
+++ precache-manifest.6e9abc66345e932ed3fe5aba54fdfd7c.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "279afb8a09031893c5b9093f83bd4391",
+    "url": "/handbased-vr-sculpting-presentation/index.html"
+  },
+  {
+    "revision": "c1548d15bf6aa1275e9c",
+    "url": "/handbased-vr-sculpting-presentation/static/css/main.9ae67dd8.chunk.css"
+  },
+  {
+    "revision": "c4cfb76e4f4be2d1fcb5",
+    "url": "/handbased-vr-sculpting-presentation/static/js/2.030bceb2.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/handbased-vr-sculpting-presentation/static/js/2.030bceb2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c1548d15bf6aa1275e9c",
+    "url": "/handbased-vr-sculpting-presentation/static/js/main.00a48b65.chunk.js"
+  },
+  {
+    "revision": "bd84828e1577c44c9c98",
+    "url": "/handbased-vr-sculpting-presentation/static/js/runtime-main.5824b316.js"
+  },
+  {
+    "revision": "e3ff62864376eff62411f687206ec1fe",
+    "url": "/handbased-vr-sculpting-presentation/static/media/c01.e3ff6286.jpg"
+  },
+  {
+    "revision": "cc54a476f39d3e3d93fac53c2d90f2b0",
+    "url": "/handbased-vr-sculpting-presentation/static/media/c02.cc54a476.jpg"
+  },
+  {
+    "revision": "42a6ee1b4f7eb2b8cacec7af780909de",
+    "url": "/handbased-vr-sculpting-presentation/static/media/c03.42a6ee1b.jpg"
+  },
+  {
+    "revision": "112feca8ebdcacc00b226667e4403284",
+    "url": "/handbased-vr-sculpting-presentation/static/media/c04.112feca8.jpg"
+  },
+  {
+    "revision": "f5dfa91423dda6d3c630352c3f31cd90",
+    "url": "/handbased-vr-sculpting-presentation/static/media/finger-slider-demo.f5dfa914.mp4"
+  },
+  {
+    "revision": "1b2331772cca3a27bbe41e0644e8bf4d",
+    "url": "/handbased-vr-sculpting-presentation/static/media/finger-slider.1b233177.svg"
+  },
+  {
+    "revision": "335beb9b30f32c8f0816f614ed609a8b",
+    "url": "/handbased-vr-sculpting-presentation/static/media/h01.335beb9b.jpg"
+  },
+  {
+    "revision": "fa04ce1d0ec96bca8801eb11cde8a8be",
+    "url": "/handbased-vr-sculpting-presentation/static/media/h02.fa04ce1d.jpg"
+  },
+  {
+    "revision": "5fe0c59198398849336591babe7f08d6",
+    "url": "/handbased-vr-sculpting-presentation/static/media/h03.5fe0c591.jpg"
+  },
+  {
+    "revision": "7f6aa40434bbf2d6ff652514eea16205",
+    "url": "/handbased-vr-sculpting-presentation/static/media/h04.7f6aa404.jpg"
+  },
+  {
+    "revision": "ce8c7145e068bb531a34f306f9548684",
+    "url": "/handbased-vr-sculpting-presentation/static/media/hsd-logos.ce8c7145.svg"
+  },
+  {
+    "revision": "c3d2d25779267e6714a29066dcbd189b",
+    "url": "/handbased-vr-sculpting-presentation/static/media/medians.c3d2d257.svg"
+  },
+  {
+    "revision": "27aebe700af44380934a70363c170d69",
+    "url": "/handbased-vr-sculpting-presentation/static/media/medium.27aebe70.jpg"
+  },
+  {
+    "revision": "7f80c0f163eddc92da9c84f50c44a15c",
+    "url": "/handbased-vr-sculpting-presentation/static/media/msmr-slider.7f80c0f1.jpg"
+  },
+  {
+    "revision": "90789171300b176c37fa37f99ec57641",
+    "url": "/handbased-vr-sculpting-presentation/static/media/portfolio.90789171.svg"
+  },
+  {
+    "revision": "464b0b46a754c848d9d35215e3751443",
+    "url": "/handbased-vr-sculpting-presentation/static/media/sculpting-demo.464b0b46.mp4"
+  },
+  {
+    "revision": "a0fd0ce85e305adc87113b3b9d583420",
+    "url": "/handbased-vr-sculpting-presentation/static/media/sphere-selector-demo.a0fd0ce8.mp4"
+  },
+  {
+    "revision": "a3d0a6b7838cc1b298467c0f50fff7cc",
+    "url": "/handbased-vr-sculpting-presentation/static/media/sphere-selector.a3d0a6b7.svg"
+  },
+  {
+    "revision": "bee11016ff9709cfde242d082b55ce0d",
+    "url": "/handbased-vr-sculpting-presentation/static/media/tiltbrush.bee11016.jpg"
+  },
+  {
+    "revision": "a76316bc1b6e85e88cc19bbb71b22728",
+    "url": "/handbased-vr-sculpting-presentation/static/media/tool-menu-demo.a76316bc.mp4"
+  },
+  {
+    "revision": "04fcf513e1c7085dba54b34527b8d624",
+    "url": "/handbased-vr-sculpting-presentation/static/media/wordpairs.04fcf513.svg"
+  }
+]);
